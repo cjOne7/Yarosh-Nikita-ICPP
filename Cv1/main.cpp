6fe6c9186b0d4d9d0e8f6480aaf2d5376a03b1b5
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-10-05 20:39:02 +0200

    Finish

--- Cv1/main.cpp
+++ Cv1/main.cpp
@@ -2,20 +2,6 @@
 
 using namespace std;
 
-void firstTask() {
-	int a, b, c;
-	cout << "Enter a:" << endl;
-	cin >> a;
-	cout << "Enter b:" << endl;
-	cin >> b;
-	cout << "Enter c:" << endl;
-	cin >> c;
-
-	if (a + b > c && a + c > b && b + c > a) {
-		cout << "Perimeter: " << a + b + c << endl;
-	}
-}
-
 struct Triangle {
 	int a;
 	int b;
@@ -30,47 +16,71 @@ bool lzeSestrojit(Triangle *t) {
 	return t->a + t->b > t->c && t->a + t->c > t->b && t->b + t->c > t->a;
 }
 
-void secondTask() {
-	Triangle triangle{};
-	cout << "Enter a: " << endl;
-	cin >> triangle.a;
-	cout << "Enter b: " << endl;
-	cin >> triangle.b;
-	cout << "Enter c: " << endl;
-	cin >> triangle.c;
+void tasks() {
+//	Triangle triangle{};
+//	cout << "Enter a: " << endl;
+//	cin >> triangle.a;
+//	cout << "Enter b: " << endl;
+//	cin >> triangle.b;
+//	cout << "Enter c: " << endl;
+//	cin >> triangle.c;
+//
+//	if (lzeSestrojit(triangle)) {
+//		cout << "Perimeter: " << triangle.a + triangle.b + triangle.c << endl;
+//	} else {
+//		cout << "Triangle can't be created with sides: "
+//			 << triangle.a << " "
+//			 << triangle.b << " "
+//			 << triangle.c << endl;
+//	}
+//	//The third task
+//	Triangle *triangle1 = &triangle;
+//	cout << "Enter a: " << endl;
+//	cin >> triangle1->a;
+//	cout << "Enter b: " << endl;
+//	cin >> triangle1->b;
+//	cout << "Enter c: " << endl;
+//	cin >> triangle1->c;
+//
+//	if (lzeSestrojit(triangle1)) {
+//		cout << "Perimeter: " << triangle1->a + triangle1->b + triangle1->c << endl;
+//	} else {
+//		cout << "Triangle can't be created with sides: "
+//			 << triangle1->a << " "
+//			 << triangle1->b << " "
+//			 << triangle1->c << endl;
+//	};
+//	delete triangle1;
+//
+//	//The forth task
+//	auto *triangle2 = new Triangle;
+//	delete triangle2;
 
-	if (lzeSestrojit(triangle)) {
-		cout << "Perimeter: " << triangle.a + triangle.b + triangle.c << endl;
-	} else {
-		cout << "Triangle can't be created with sides: "
-			 << triangle.a << " "
-			 << triangle.b << " "
-			 << triangle.c << endl;
-	}
-	//The third task
-	Triangle *triangle1 = &triangle;
-	cout << "Enter a: " << endl;
-	cin >> triangle1->a;
-	cout << "Enter b: " << endl;
-	cin >> triangle1->b;
-	cout << "Enter c: " << endl;
-	cin >> triangle1->c;
-
-	if (lzeSestrojit(triangle1)) {
-		cout << "Perimeter: " << triangle1->a + triangle1->b + triangle1->c << endl;
-	} else{
-		cout << "Triangle can't be created with sides: "
-			 << triangle1->a << " "
-			 << triangle1->b << " "
-			 << triangle1->c << endl;
-	};
-	delete triangle1;
-
-	//The forth task
-	auto *triangle2 = new Triangle;
-	delete triangle2;
 	//The fifth task
+	int numberOfTriangles;
+	cout << "Enter number of triangles: " << endl;
+	cin >> numberOfTriangles;
+	auto *triangles = new Triangle[numberOfTriangles];
+	for (int i = 0; i < numberOfTriangles; i++) {
+		cout << "Enter a: " << endl;
+		cin >> triangles[i].a;
+		cout << "Enter b: " << endl;
+		cin >> triangles[i].b;
+		cout << "Enter c: " << endl;
+		cin >> triangles[i].c;
 
+		if (lzeSestrojit(triangles[i])) {
+			cout << "Perimeter of triangle number " << i + 1 << " is "
+				 << triangles[i].a + triangles[i].b + triangles[i].c;
+		} else {
+			cout << "Triangle can't be created with sides: "
+				 << triangles[i].a << " "
+				 << triangles[i].b << " "
+				 << triangles[i].c << endl;
+		}
+	}
+
+	delete[] triangles;
 
 }
 
