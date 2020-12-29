83628d558490b5f0a05eec6ee54b677634a18b5f
cjOne7 <61429321+cjOne7@users.noreply.github.com>
2020-12-29 21:10:55 +0100

    fix

--- Sem JsonLibrary ArrayList/main.cpp
+++ Sem JsonLibrary ArrayList/main.cpp
@@ -4,37 +4,23 @@
 #include "Student.h"
 #include <fstream>
 
+string buildOutputString(DynamicArray<Student *> *dynamicStudentsArray) {
+	stringstream ss;
+	cout << endl;
+	ss << "{\"users\":[";
+	for (int i = 0; i < dynamicStudentsArray->getSize(); ++i) {
+		//			cout << *dynamicStudentsArray->getElementAt(i) << endl;
+		ss << *dynamicStudentsArray->getElementAt(i);
+		if (i != dynamicStudentsArray->getSize() - 1) {
+			ss << ',';
+		}
+	}
+	ss << "]}";
+	return ss.str();
+}
+
 int main() {
 	try {
-//		Value *value = JSON::deserialize(
-//				"{\"str\": \"Hello, !@#$%^&*()_/*-+=1234567890:;|,.<> \' \\ \" World?!\", \"str\": \"Hello, \'\\\"World?!\"}");
-//just test
-//		Value *value = JSON::deserialize(
-//				"{\"id\":1,\"name\":\"Nikita\",\"surname\":\"Yarosh\",\"year\":3,\"credits\":30,\"address\":{\"city\":\"Pardubice\",\"street\":\"Studentska 199\",\"post code\":53009},\"subjects' list\":[]}");
-//		Value *value = JSON::deserialize(
-//				"{ \"alpha\":{      \"beta\" : 100 , \"gamma\":200},\"delta\" : [ 0 , 1 , 2 ,3      ,4  ,null      , { \"a\"          :\"b\",\"c\":    { }        ,\"d\": [  ] ,\"e\":true,\"f\":false,\"g\":[{ },{ }, {}, [],[ ]]  } ] }");
-//test objects sequence
-//		Value *value = JSON::deserialize(
-//				"{\"boolean\":true,\"obj1\":{\"obj_value1\":10,\"inner_obj1\":{},\"arr\":[1]}}");
-//test objects sequences and inner objects
-//		Value *value = JSON::deserialize(
-//				"{\"obj1\":{\"obj_value1\":1 ,\"inner_obj1\":{\"inner_obj_value1\":100},\"inner_obj2\":{\"inner_obj_value2\":200}},\"obj2\":{\"obj_value2\":20}}");
-//test arrays sequence
-//		Value *value = JSON::deserialize(
-//				"{\"boolean\":true,\"arr1\":[1,true],\"arr2\":[2,false]}");
-//test inner objects in objects and arrays in arrays
-//		Value *value = JSON::deserialize(
-//				"{\"boolean\":true,\"arr\":[1,true,[null, 1.2],[]],\"obj\":{\"obj_value\":123,\"inner_obj\":{\"inner_obj_value\":\"inner string\"}}}");
-//test primitives
-//		Value *value = JSON::deserialize(
-//				"{\"boolean\":true, \"boolean1\":false, \"num asdd\":123.2,\"null_value\":null, \"str\":\" some, string\"}");
-//array of objects
-//		Value *value = JSON::deserialize(
-//				"{\"users\":[{\"id\":1,\"name\":\"Nikita\",\"surname\":\"Yarosh\",\"year\":3,\"credits\":30,\"address\":{\"city\":\"Pardubice\",\"street\":\"Studentska 199\",\"post code\":53009},\"subjects' list\":[\"IMOSI\",\"IWWW\",\"ICPP\",\"IDAS2\"]},{\"id\":2,\"name\":\"Vlad\",\"surname\":\"Ermolaev\",\"year\":3,\"credits\":30,\"address\":{\"city\":\"Kolin\",\"street\":\"Studentska 199\",\"post code\":53009},\"subjects' list\":[\"IMOSI\",\"IWWW\",\"ICPP\",\"IDAS2\"]},{\"id\":3,\"name\":\"Andrii\",\"surname\":\"Andrusenko\",\"year\":3,\"credits\":30,\"address\":{\"city\":\"Prelouc\",\"street\":\"Studentska 199\",\"post code\":53009},\"subjects' list\":[\"IMOSI\",\"IWWW\",\"ICPP\",\"IDAS2\"]},{\"id\":4,\"name\":\"Dmytro\",\"surname\":\"Hrychanok\",\"year\":3,\"credits\":30,\"address\":{\"city\":\"Pardubice\",\"street\":\"Studentska 199\",\"post code\":53009},\"subjects' list\":[\"IMOSI\",\"IWWW\",\"ICPP\",\"IDAS2\"]}]}");
-//test all
-//		Value *value = JSON::deserialize(
-//				"{\"v_true\":true,\"v_false\":false,\"v_null\":null,\"v_int\":123,\"v_double\":1.23,\"v_string\":\"Hello, World\",\"v_array\":[1,2.3,true,false,null,{},[]]}");
-//		cout << JSON::serialize(value);
 //		ofstream fileWriter{};
 //		fileWriter.open("students.json");
 //		if (fileWriter.is_open()) {
@@ -99,17 +85,17 @@ int main() {
 			}
 		}
 //		cout << "Select user:" << endl;
-		stringstream ss;
-		cout << endl;
-		ss << "{\"users\":[";
-		for (int i = 0; i < dynamicStudentsArray->getSize(); ++i) {
-//			cout << *dynamicStudentsArray->getElementAt(i) << endl;
-			ss << *dynamicStudentsArray->getElementAt(i);
-			if (i != dynamicStudentsArray->getSize() - 1) {
-				ss << ',';
-			}
-		}
-		ss << "]}";
+//		stringstream ss;
+//		cout << endl;
+//		ss << "{\"users\":[";
+//		for (int i = 0; i < dynamicStudentsArray->getSize(); ++i) {
+////			cout << *dynamicStudentsArray->getElementAt(i) << endl;
+//			ss << *dynamicStudentsArray->getElementAt(i);
+//			if (i != dynamicStudentsArray->getSize() - 1) {
+//				ss << ',';
+//			}
+//		}
+//		ss << "]}";
 //		cout << ss.str() << endl;
 
 //		int command = 0;
@@ -134,7 +120,7 @@ int main() {
 		ofstream fileWriter{};
 		fileWriter.open("students.json");
 		if (fileWriter.is_open()) {
-			fileWriter << JSON::serialize(JSON::deserialize(ss.str())) << endl;
+			fileWriter << JSON::serialize(JSON::deserialize(buildOutputString(dynamicStudentsArray))) << endl;
 			fileWriter.close();
 		}
 
